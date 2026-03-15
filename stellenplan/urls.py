c964f0b255027b4a37070ee7c7f39210fc15ebc8
hagenderhenker <md1234567890@web.de>
2026-03-15 21:42:43 +0100

    CRUD Views + Urls als Dummy angelegt Stellenplan

--- stellenplan/urls.py
+++ stellenplan/urls.py
@@ -16,9 +16,62 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . views import stellenuebersicht
+from . views import stellenarten_uebersicht, stellenarten_add, stellenarten_edit, stellenarten_delete
+from . views import haushalte_uebersicht, haushalte_add, haushalte_edit, haushalte_delete
+from . views import teilhaushalte_uebersicht, teilhaushalte_add, teilhaushalte_edit, teilhaushalte_delete
+from . views import produkte_uebersicht, produkte_add, produkte_edit, produkte_delete
+from . views import kostenstellen_uebersicht, kostenstellen_add, kostenstellen_edit, kostenstellen_delete
+from . views import stellenuebersicht, stellen_add, stellen_edit, stellen_delete
+from . views import kostenstellen_uebersicht, kostenstellen_add, kostenstellen_edit, kostenstellen_delete
+from . views import stellenplan_soll_uebersicht, stellenplan_soll_add, stellenplan_soll_edit, stellenplan_soll_delete
+from . views import stellenplan_ist_uebersicht, stellenplan_ist_add, stellenplan_ist_edit, stellenplan_ist_delete
 
 urlpatterns = [
-    path('', stellenuebersicht, name='stellenuebersicht'),
+    # CRUD Stellenarten model = Stellenarten
+    path('stellenarten/', stellenarten_uebersicht, name='stellenarten_uebersicht'),
+    path('stellenarten/add/', stellenarten_add, name='stellenarten_add'),
+    path('stellenarten/edit/<int:id>/', stellenarten_edit, name='stellenarten_edit'),
+    path('stellenarten/delete/<int:id>/', stellenarten_delete, name='stellenarten_delete'),
 
+    # CRUD Haushalte
+    path('haushalte/', haushalte_uebersicht, name='haushalte_uebersicht'),
+    path('haushalte/add/', haushalte_add, name='haushalte_add'),   
+    path('haushalte/edit/<int:id>/', haushalte_edit, name='haushalte_edit'),
+    path('haushalte/delete/<int:id>/', haushalte_delete, name='haushalte_delete'),
+
+    # CRUD Teilhaushalte model = Teilhaushalte
+    path('teilhaushalte/', teilhaushalte_uebersicht, name='teilhaushalte_uebersicht'),
+    path('teilhaushalte/add/', teilhaushalte_add, name='teilhaushalte_add'),
+    path('teilhaushalte/edit/<int:id>/', teilhaushalte_edit, name='teilhaushalte_edit'),
+    path('teilhaushalte/delete/<int:id>/', teilhaushalte_delete, name='teilhaushalte_delete'),
+    
+    # CRUD Produkte model = Produkte
+    path('produkte/', produkte_uebersicht, name='produkte_uebersicht'),
+    path('produkte/add/', produkte_add, name='produkte_add'),
+    path('produkte/edit/<int:id>/', produkte_edit, name='produkte_edit'),
+    path('produkte/delete/<int:id>/', produkte_delete, name='produkte_delete'),
+    
+    # CRUD Kostenstellen model = Kostenstellen
+    path('kostenstellen/', kostenstellen_uebersicht, name='kostenstellen_uebersicht'),
+    path('kostenstellen/add/', kostenstellen_add, name='kostenstellen_add'),
+    path('kostenstellen/edit/<int:id>/', kostenstellen_edit, name='kostenstellen_edit'),
+    path('kostenstellen/delete/<int:id>/', kostenstellen_delete, name='kostenstellen_delete'),
+    
+    # CRUD Stellen model = Stellen
+    path('', stellenuebersicht, name='stellenuebersicht'),
+    path('stellen/add/', stellen_add, name='stellen_add'),
+    path('stellen/edit/<int:id>/', stellen_edit, name='stellen_edit'),  
+    path('stellen/delete/<int:id>/', stellen_delete, name='stellen_delete'),
+    
+    # CRUD Stellenplan Soll model = Stellenplan_Soll
+    path('stellenplan_soll/', stellenplan_soll_uebersicht, name='stellenplan_soll_uebersicht'),
+    path('stellenplan_soll/add/', stellenplan_soll_add, name='stellenplan_soll_add'),
+    path('stellenplan_soll/edit/<int:id>/', stellenplan_soll_edit, name='stellenplan_soll_edit'),
+    path('stellenplan_soll/delete/<int:id>/', stellenplan_soll_delete, name='stellenplan_soll_delete'),
+    
+    # CRUD Stellenplan Ist model = Stellenplan_Ist
+    path('stellenplan_ist/', stellenplan_ist_uebersicht, name='stellenplan_ist_uebersicht'),
+    path('stellenplan_ist/add/', stellenplan_ist_add, name='stellenplan_ist_add'),
+    path('stellenplan_ist/edit/<int:id>/', stellenplan_ist_edit, name='stellenplan_ist_edit'),
+    path('stellenplan_ist/delete/<int:id>/', stellenplan_ist_delete, name='stellenplan_ist_delete'),    
 ]
--- stellenplan/views.py
+++ stellenplan/views.py
@@ -1,7 +1,136 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+
 # Create your views here.
+
+#CRUD Stellenarten model = Stellenarten
+
+def stellenarten_uebersicht(request):
+    return HttpResponse("Stellenartenübersicht")
+
+def stellenarten_add(request):
+    return HttpResponse("Stellenart hinzufügen")
+
+def stellenarten_edit(request, id):
+    return HttpResponse(f"Stellenart mit ID {id} bearbeiten")
+
+def stellenarten_delete(request, id):
+    return HttpResponse(f"Stellenart mit ID {id} löschen")
+
+# CRUD Haushalte
+
+def haushalte_uebersicht(request):
+    return HttpResponse("Haushaltsübersicht")
+
+def haushalte_add(request):
+    return HttpResponse("Haushalt hinzufügen")
+
+def haushalte_edit(request, id):
+    return HttpResponse(f"Haushalt mit ID {id} bearbeiten")
+
+def haushalte_delete(request, id):
+    return HttpResponse(f"Haushalt mit ID {id} löschen")
+
+# CRUD Teilhaushalte model = Teilhaushalte
+
+def teilhaushalte_uebersicht(request):
+    return HttpResponse("Teilhaushaltsübersicht")
+
+def teilhaushalte_add(request):
+    return HttpResponse("Teilhaushalt hinzufügen")
+
+def teilhaushalte_edit(request, id):
+    return HttpResponse(f"Teilhaushalt mit ID {id} bearbeiten")
+
+def teilhaushalte_delete(request, id):
+    return HttpResponse(f"Teilhaushalt mit ID {id} löschen")
+
+# CRUD Produkte model = Produkte
+
+def produkte_uebersicht(request):
+    return HttpResponse("Produktübersicht")
+
+def produkte_add(request):
+    return HttpResponse("Produkt hinzufügen")
+
+def produkte_edit(request, id):
+    return HttpResponse(f"Produkt mit ID {id} bearbeiten")
+
+def produkte_delete(request, id):
+    return HttpResponse(f"Produkt mit ID {id} löschen")
+
+# CRUD Kostenstellen model = Kostenstellen
+
+def kostenstellen_uebersicht(request):
+    return HttpResponse("Kostenstellenübersicht")
+
+def kostenstellen_add(request):
+    return HttpResponse("Kostenstelle hinzufügen")
+
+def kostenstellen_edit(request, id):
+    return HttpResponse(f"Kostenstelle mit ID {id} bearbeiten")
+
+def kostenstellen_delete(request, id):
+    return HttpResponse(f"Kostenstelle mit ID {id} löschen")
+
+
+# CRUD Stellenplan model = Stellenplan
 
 def stellenuebersicht(request):
     return HttpResponse("Stellenübersicht")
+
+def stellen_add(request):
+    return HttpResponse("Stelle hinzufügen")
+
+def stellen_edit(request, id):
+    return HttpResponse(f"Stelle mit ID {id} bearbeiten")
+
+def stellen_delete(request, id):
+    return HttpResponse(f"Stelle mit ID {id} löschen")
+
+
+# CRUD Kostenstellenaufteilung model = KostenstellenausteilungStellen
+
+def kostenstellenausteilung_uebersicht(request):
+    return HttpResponse("Kostenstellenausteilungsübersicht")
+
+def kostenstellenausteilung_add(request):
+    return HttpResponse("Kostenstellenausteilung hinzufügen")
+
+def kostenstellenausteilung_edit(request, id):
+    return HttpResponse(f"Kostenstellenausteilung mit ID {id} bearbeiten")
+
+def kostenstellenausteilung_delete(request, id):
+    return HttpResponse(f"Kostenstellenausteilung mit ID {id} löschen")
+
+# CRUD Stellenplan Soll model = StellenplanSoll
+def stellenplan_soll_uebersicht(request):
+    return HttpResponse("Stellenplan Soll Übersicht")
+
+def stellenplan_soll_add(request):
+    return HttpResponse("Stellenplan Soll hinzufügen")
+
+def stellenplan_soll_edit(request, id):
+    return HttpResponse(f"Stellenplan Soll mit ID {id} bearbeiten")
+
+def stellenplan_soll_delete(request, id):
+    return HttpResponse(f"Stellenplan Soll mit ID {id} löschen")
+
+# CRUD Stellenplan Ist model = StellenplanIst
+def stellenplan_ist_uebersicht(request):
+    return HttpResponse("Stellenplan Ist Übersicht")
+
+def stellenplan_ist_add(request):
+    return HttpResponse("Stellenplan Ist hinzufügen")
+
+def stellenplan_ist_edit(request, id):
+    return HttpResponse(f"Stellenplan Ist mit ID {id} bearbeiten")
+
+def stellenplan_ist_delete(request, id):
+    return HttpResponse(f"Stellenplan Ist mit ID {id} löschen")
+
+
+
+
+
